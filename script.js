8541bbf629ecbc9cf7e156d3c2248ed03937e928
osvaldoarreola <37843538+osvaldoarreola@users.noreply.github.com>
2019-11-20 15:33:41 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+a = document.getElementById("cat");
+b = document.getElementById("dog");
+c = document.getElementById("fish");
+d = document.getElementById("bird");
+e = document.getElementById("left");
+f = document.getElementById("right");
+g = document.getElementById("ant");
+
+function switchOne(){
+	a.classList.remove("gone");
+	g.classList.remove("gone");
+	a.src = "images/blueb.png";
+	g.innerHTML = "This piece was one of the first designs I created. It started of with the L being a worm character later on changing it tho the letter L. From there I thought of words that started with the letter and came with LIKE. I made the letters and saw my paint makers and decided to layer the colors based on a tropical birds feathers. The border on the text is there to be a digital design to give the text flavor."
+}
+function switchTwo(){
+	a.classList.remove("gone");
+	g.classList.remove("gone");
+	a.src = "images/cash.png";
+	g.innerHTML = "The cash piece was inspired based off of well money. I was thinking of what people would like to see in a word and what people are drawn to. So the word came to me and I wanted to make the colors for it an apple green and a bold black. The dots are there for a simple design around so the text doesn't look so plain."
+}
+function switchThird(){
+	a.classList.remove("gone");
+	g.classList.remove("gone");
+	a.src = "images/jazz2.png";
+	g.innerHTML = "This one was based on my nieces name which is Jasmine. We like to call her jazz and she likes cherries. The colorway was based off of a Koi-Fish pattern. I also wanted the words to flow like music because of the play on words being a music jenare that why the J has a curve. The waves on the top are to symbolize a zap."
+}
+function switchFour(){
+	a.classList.remove("gone");
+	g.classList.remove("gone");
+	a.src = "images/hunger.png";
+	g.innerHTML = "The Hunger piece is detected to my artist name. The color ways are blue and purple the reason for this is because purple symbolizes the void and blue is my favorite color pink was there to put in a pop up with a shine. The smile in the guy is just how I feel inside. The blocks outside are to show that life is simple and don't box yourself out of it."
+}
